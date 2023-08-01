acfa1630fed32545ee4fd66562bd6dea23ea6c20
dziakola <magdalena.dziakola@gmail.com>
2023-08-01 19:10:20 +0200

    update

--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -84,7 +84,8 @@ export const removeTableRequest = (id) => {
     }
     fetch(`${API_URL}/tables/${id}`, options)
       .then((res) => res.json())
-      .then((table) => dispatch(()=>removeTable(table.id)));
+      .then(table=> console.log(table))
+      .then((table) => dispatch(removeTable(table.id)))
   };
 };
 
